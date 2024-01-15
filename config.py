568d167bfaa39d43968caad7ac3b49af05b9a5b4
Wiko De La Rosa <wico216@gmail.com>
2024-01-15 14:50:21 -0500

    Update bot token and client secret

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ MUSIC_CHANNEL_ID = 603687336013725714 # This is the id of the channel where the 
 ROLE_ID = 1192606417958469673 # This is the id of the role that will be able to use the bot Currently: @Discord Bot
 CLIENT_ID = '1192578363806732400' # This is your bots id
 OWNER_ID = '239228553428729856' # This is your user id
-BOT_TOKEN = 'MTE5MjU3ODM2MzgwNjczMjQwMA.GkyqIc.qfEp1EGlEu097tn-qrjkC7QPArMieO52Ek9Bec' # This is the token for the bot
-CLIENT_SECRET = "0mMaHMDhBzBBK56c5E5seX8jZXGTxfh3" # This is the client secret for the bot
+BOT_TOKEN = 'MTE5MjU3ODM2MzgwNjczMjQwMA.GVoiQh.rPa3sSwKnIUyQZkkrEs7wzFcH_9FOxZKtj9kXI' # This is the token for the bot
+CLIENT_SECRET = "tegR-F62odRkvMexlRleIv4RkW0I5liK" # This is the client secret for the bot
 FFMPEG_EXECUTABLE_PATH = 'C:/ffmpeg/bin/ffmpeg.exe' # This is the path to the ffmpeg executable 
 
 
